3def0e1f021397357995bbba61e06bd4b0fbe216
vasili.kostin <vasili.kostin@bgaming.com>
2021-07-27 18:12:08 +0300

    spinner style.css

--- www/js/loading-indicator.js
+++ www/js/loading-indicator.js
@@ -2,18 +2,6 @@
 
 import {isLitePage, renderIcon} from "./utils.js";
 
-
-var backDropStyle = {
-	position: 'fixed',
-	top: 0,
-	bottom: 0,
-	background: "#00102000",
-	right: 0,
-	left: 0,
-	zIndex: 15,
-	cursor: "wait"
-};
-
 export default class LoadingIndicator extends React.Component {
 
 	constructor(props) {
@@ -36,16 +24,15 @@ export default class LoadingIndicator extends React.Component {
 	}
 
 	render() {
-		if(this.state.showCount > 0) {
-			return ReactDOM.div({style: backDropStyle},
-				ReactDOM.div({style: {margin: 'auto', position: 'relative', border: '2px solid #fff', borderTop: 0, height: '40px', width: '90px', transition: '0.3s', overflow: 'hidden', background: '#999', borderBottomLeftRadius: '4px', borderBottomRightRadius: '4px'}},
-					ReactDOM.div({style: {position: 'absolute', width: '90px', textAlign: 'center', color: '#fff', fontSize: '170%', bottom: '3px'}},
-						renderIcon('cog fa-spin')
-					)
-				)
-			);
-		} else {
-			return ReactDOM.div();
-		}
+		let active = this.state.showCount > 0;
+
+		return ReactDOM.div({className: active ? 'back-drop' : null},
+			ReactDOM.div({className: active ? "loading-spinner-container" : "loading-spinner-container-inactive"},
+				active ? ReactDOM.div({className: "loading-spinner"},
+					renderIcon('cog fa-spin')
+				) : undefined
+			)
+		);
+
 	}
 }
