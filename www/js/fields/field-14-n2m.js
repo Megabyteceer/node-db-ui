a83ae37aa06415af67cf71e6fe32c592d16aef3b
vasili.kostin <vasili.kostin@bgaming.com>
2021-07-29 18:50:31 +0300

    lookup n2m style css

--- www/js/fields/field-14-n2m.js
+++ www/js/fields/field-14-n2m.js
@@ -43,10 +43,6 @@ registerFieldClass(FIELD_14_NtoM, class LookupNtoMField extends fieldLookupMixin
 		}
 	}
 
-	isDividerItem(v, field) {
-		return this.state.extendedEditor && v && (v.icon === '111111');
-	}
-
 	dragStart(item) {
 		if(!dragListenersInited) {
 			$(document).on('mouseup', () => {
@@ -71,6 +67,7 @@ registerFieldClass(FIELD_14_NtoM, class LookupNtoMField extends fieldLookupMixin
 
 					dragList.state.value.some((i) => {
 						var el = ReactDOM.findDOMNode(refs[UID(i)]);
+						// @ts-ignore
 						var ey = el.getBoundingClientRect();
 						ey = (ey.top + ey.bottom) / 2;
 
@@ -103,85 +100,37 @@ registerFieldClass(FIELD_14_NtoM, class LookupNtoMField extends fieldLookupMixin
 		this.forceUpdate();
 	}
 
-	renderItem(field, v, i, isEdit) {
+	renderItem(field, value, i, isEdit) {
 
-		var isDrag = (dragItem === v);
+		var isDrag = (dragItem === value);
 		var buttons;
 		var isNew = '';
-		if(!v) {
+		if(!value) {
 			isNew = 'n';
 		}
 		var additionalButtonsN2M = this.state.additionalButtonsN2M || this.props.additionalButtonsN2M;
 		if(additionalButtonsN2M) {
-			additionalButtonsN2M = additionalButtonsN2M(field, v, i, this);
+			additionalButtonsN2M = additionalButtonsN2M(field, value, i, this);
 		}
 
 		if(isEdit) {
-			if(this.state.extendedEditor) {
-				if(v) {
-
-					buttons = R.div({style: {width: '30%', display: 'inline-block', verticalAlign: 'middle', textAlign: 'right'}},
-						additionalButtonsN2M,
-						/*R.button({style:{background: window.constants.EDIT_COLOR, color:'#fff'}, title:L('MOVE_UP'), className:'clickable toolbtn', onClick:() => {
-								
-								if (i > 0) {
-									var t = this.state.value[i];
-									this.state.value[i] = this.state.value[i-1];
-									this.state.value[i-1] = t;
-									this.forceUpdate();
-								}
-							}},
-							renderIcon('arrow-up')
-						),
-						R.button({style:{background: window.constants.EDIT_COLOR, color:'#fff'}, title:L('MOVE_DOWN'), className:'clickable toolbtn', onClick:() => {
-								if(i < (this.state.value.length-1)){
-									var t = this.state.value[i];
-									this.state.value[i] = this.state.value[i+1];
-									this.state.value[i+1] = t;
-									this.forceUpdate();
-								}
-								
-							}},
-							renderIcon('arrow-down')
-						),*/
-						R.button({
-							style: {background: window.constants.EDIT_COLOR, color: '#fff'}, title: L('EDIT'), className: 'clickable toolbtn', onClick: () => {
-								this.uidToEdit = UID(this.state.value[i]);
-								this.forceUpdate();
-							}
-						},
-							renderIcon('pencil')
-						),
-						R.button({
-							style: {background: window.constants.DELETE_COLOR, color: '#fff'}, title: L('LIST_REMOVE'), className: 'clickable toolbtn', onClick: () => {
-								this.deleteItemByIndex(i);
-
-							}
-						},
-							renderIcon('times')
-						),
-						R.div({
-							style: {color: '#bbb', fontSize: '140%', verticalAlign: 'middle', display: 'inline-block'}, className: isDrag ? 'drag' : 'draggable', onMouseDown: (e) => {
-								sp(e);
-								this.dragStart(v);
-							}
-						}, renderIcon('reorder'))
-					)
-				} else {
-					/*buttons = R.div({style:{width:'20%', display:'inline-block', fontSize:'50%', verticalAlign:'middle', textAlign:'left'}, className:'halfvisible'},
-						R.button({style:{background:'#0a7', color:'#fff'}, title:L('ADD_DIVIDER'), className:'clickable toolbtn', onClick:() => {
-								this.state.value.splice(i,0,{id:0, name:L('NEW_GROUP')});
-								this.forceUpdate();
-							}},
-							renderIcon('plus'), L('DIVIDER')
-						)
-					)*/
+			if(value) {
+				let reorderButton;
+				if(this.state.extendedEditor) {
+					reorderButton = R.button({
+						title: L('FRAG_TO_REORDER'),
+						className: isDrag ? 'toolbtn drag draggable default-btn' : 'toolbtn drag default-btn', onMouseDown: (e) => {
+							sp(e);
+							this.dragStart(value);
+						}
+					}, renderIcon('reorder'));
 				}
-			} else if(v) {
-				buttons = R.div({style: {width: '30%', display: 'inline-block', fontSize: '60%', verticalAlign: 'middle', textAlign: 'left'}, className: 'halfvisible'},
+
+				buttons = R.span({className: 'field-lookup-right-block'},
 					additionalButtonsN2M,
 					R.button({
-						style: {background: window.constants.EDIT_COLOR, color: '#fff'}, title: L('EDIT'), className: 'clickable toolbtn', onClick: () => {
+						title: L('EDIT'),
+						className: 'clickable toolbtn edit-btn', onClick: () => {
 							this.uidToEdit = UID(this.state.value[i]);
 							this.forceUpdate();
 						}
@@ -189,60 +138,45 @@ registerFieldClass(FIELD_14_NtoM, class LookupNtoMField extends fieldLookupMixin
 						renderIcon('pencil')
 					),
 					R.button({
-						style: {background: window.constants.DELETE_COLOR, color: '#fff'}, title: L('LIST_REMOVE'), className: 'clickable toolbtn', onClick: () => {
+						title: L('LIST_REMOVE'),
+						className: 'clickable toolbtn danger-btn', onClick: () => {
 							this.deleteItemByIndex(i);
 						}
 					},
 						renderIcon('times')
-					)
+					),
+					reorderButton
 				)
 			}
-		}
 
-		if(isEdit || v) {
-
-			var editIt = v && this.uidToEdit === UID(v);
-			if(editIt) {
-				delete (this.uidToEdit);
-				editIt = v.id;
-			}
-
-			var borderBottom;
-
-			var key;
-			if(v) {
-				key = UID(v);
-
-				if(!this.isDividerItem(v, field)) {
-					borderBottom = '1px solid #ccc';
+			if(isEdit || value) {
+				var editIt = value && this.uidToEdit === UID(value);
+				if(editIt) {
+					delete (this.uidToEdit);
+					editIt = value.id;
 				}
-			} else {
-				key = 'emp' + keyCounter;
-				keyCounter++;
-			}
+				var key;
+				if(value) {
+					key = UID(value);
+				} else {
+					key = 'emp' + keyCounter;
+					keyCounter++;
+				}
+				var body = R.div({key: key, ref: value ? (ref) => {refs[UID(value)] = ref;} : undefined, className: isDrag ? 'lookup-n2m-item lookup-n2m-item-drag' : 'lookup-n2m-item'},
 
-			var body = R.div({key: key, ref: v ? (ref) => {refs[UID(v)] = ref;} : undefined, style: {padding: '2px 20px', borderBottom: borderBottom, outline: isDrag ? '3px solid #0d5' : undefined}},
-				R.div({style: {width: '70%', display: 'inline-block'}},
 					React.createElement(getClassForField(FIELD_7_Nto1), {
-						field, preventCreateButton: this.state.preventCreateButton, editIt, hideIcon: this.isDividerItem(v, field), pos: i, isEdit, isN2M: true, filters: this.state.filters, noBorder: true, ref: (ref) => {
+						field, preventCreateButton: this.state.preventCreateButton, editIt, pos: i, isEdit, isN2M: true, filters: this.state.filters, ref: (ref) => {
 							if(ref) {
 								ref.setLookupFilter({'exludeIDs': this.exludeIDs || this.state.filters.exludeIDs});
 							}
-						}, isNew: isNew, wrapper: this, initialValue: v, isCompact: this.props.isCompact, fieldDisabled: this.props.fieldDisabled
-					})
-				),
-				buttons
-			);
-
-			if(this.isDividerItem(v, field)) {
-				return R.div({key: key, noBorder: true, style: {background: '#eee', fontWeight: 'bold', padding: '5px', marginTop: (i === 0) ? 0 : '40px'}},
-					body
-				)
-			} else {
+						}, isNew: isNew, wrapper: this, initialValue: value, isCompact: this.props.isCompact, fieldDisabled: this.props.fieldDisabled
+					}),
+					buttons
+				);
 				return body;
+			} else {
+				return undefined;
 			}
-		} else {
-			return undefined;
 		}
 	}
 
@@ -267,17 +201,13 @@ registerFieldClass(FIELD_14_NtoM, class LookupNtoMField extends fieldLookupMixin
 		this.exludeIDs = exludeIDs;
 
 		var lines = [];
-		value.forEach((v, i) => {
-			if(i !== 0 && this.isDividerItem(v, field)) {
-				lines.push(this.renderItem(field, null, i, this.props.isEdit));
-			}
-			lines.push(this.renderItem(field, v, i, this.props.isEdit));
+		value.forEach((value, i) => {
+			lines.push(this.renderItem(field, value, i, this.props.isEdit));
 		});
 
 		lines.push(this.renderItem(field, null, lines.length, this.props.isEdit));
 
 		return R.div(null,
-			//R.h3({key:'header', style:{marginBottom:15}}, field.name),
 			lines
 		);
 
